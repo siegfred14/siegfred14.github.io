a2713ce353257e3933a58a0938b7e8089043713f
Siegfred Samson <siegfredsamson@gmail.com>
2021-06-02 08:17:09 +0100

    process for mainEl, anchorEl and Wrapper El, listened in submitEl index.js

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ let submitEl = document.querySelector(".submit");
 let anchorEl = document.querySelector(".nav-list");
 
 submitEl.addEventListener("click", function(){
-    
+    mainEl.classList.remove("main-edited");
+    anchorEl.classList.remove("nav-list2");
+    wrapperEl.classList.add("main-edited");
 });
