1a8f32a751b1b59518d82fc15f78c1e74b15d9f6
Siegfred Samson <siegfredsamson@gmail.com>
2021-06-02 08:12:39 +0100

    elements .submit and .nav-list selected in index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let wrapperEl = document.querySelector(".wrapper");
 let mainEl = document.querySelector(".main-content");
+let submitEl = document.querySelector(".submit");
+let anchorEl = document.querySelector(".nav-list");
 
